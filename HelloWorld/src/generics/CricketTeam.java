44071aa9f6d8f50471d377f3c5ff7915528a4aa1
AswinkumarGP <38528686+AswinkumarGP@users.noreply.github.com>
2022-09-04 21:28:12 +0200

    Delete HelloWorld/src directory
    Delete

--- HelloWorld/src/generics/CricketTeam.java
+++
@@ -1,8 +0,0 @@
-package generics;
-
-public class CricketTeam extends Team implements Comparable<Team>{
-
-    public CricketTeam(String teamName, Integer position) {
-        super(teamName, position);
-    }
-}
